efa5b89da47f3a76fd99c99afed787a67fded272
Aleksandr86rus <168619858+Aleksandr86rus@users.noreply.github.com>
2026-03-06 11:04:01 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,20 @@
 const CACHE_NAME = 'shift-calendar-v1';
 const ASSETS = [
-  'index.html',
-  'style.css',
-  'app.js',
-  'manifest.json'
+  '/calendar_Smen/',
+  '/calendar_Smen/index.html',
+  '/calendar_Smen/manifest.json'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
