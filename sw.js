411d365c08c5491b94737836f795d49aa1e98188
Aleksandr86rus <168619858+Aleksandr86rus@users.noreply.github.com>
2026-03-06 12:16:57 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,21 @@
 const CACHE_NAME = 'shift-v1';
-const ASSETS = ['index.html', 'manifest.json'];
+const ASSETS = [
+  './',
+  'index.html',
+  'manifest.json'
+];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
+
